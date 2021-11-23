7a5f10bb8fedeb2451a3162d1dc949097d168818
vidhyani <vidhyanigandhi@gmail.com>
2021-11-23 20:20:51 +0530

    changes

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@
 var bg, backgroundImg;
 var ironman,ironmanImg;
 function preload() {
-  backgroundImg = loadImage("images/bg.jpg");
+  backgroundImg = loadImage("images/bg2.jpg");
   ironmanImg = loadImage("images/iron.png")
 }
 
@@ -13,16 +13,13 @@ function setup() {
  ironman= createSprite(100,500,20,70);
  ironman.addImage(ironmanImg);
  ironman.scale=0.3
- ground=createSprite(200,585,400,10)
-ground.visible=false;
- 
 }
 
 function draw() {
 
 bg.velocityY=5;
-if(bg.y<150){
-  bg.y=500
+if(bg.y>350){
+  bg.y=200
 }
 
  if(keyDown("up")){
@@ -35,7 +32,6 @@ if(keyDown("right")){
   ironman.x =ironman.x + 5;
 } 
 ironman.velocityY = ironman.velocityY + 0.5
-ironman.collide(ground)
     
     drawSprites();
    
